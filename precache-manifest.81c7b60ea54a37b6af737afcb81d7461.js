fe58417554c0d679de604342c55573ce10d88a4b
Tenson Cai <tensoncai@Yings-Air-9.lan>
2020-05-24 12:33:50 -0500

    Updates

---
+++ precache-manifest.81c7b60ea54a37b6af737afcb81d7461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5817b43cf34471519e25a5b4283005",
+    "url": "/Birthdays/index.html"
+  },
+  {
+    "revision": "c33809d27a909cc9cbaa",
+    "url": "/Birthdays/static/css/main.50f22096.chunk.css"
+  },
+  {
+    "revision": "4c66f0746990f5a648e8",
+    "url": "/Birthdays/static/js/2.c4465b07.chunk.js"
+  },
+  {
+    "revision": "6485e28f1bc40c1e743bb4de5f286310",
+    "url": "/Birthdays/static/js/2.c4465b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33809d27a909cc9cbaa",
+    "url": "/Birthdays/static/js/main.787e4785.chunk.js"
+  },
+  {
+    "revision": "014e5a11ae25a808219b",
+    "url": "/Birthdays/static/js/runtime-main.e34454f2.js"
+  }
+]);
